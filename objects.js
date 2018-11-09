9d38bf3033a8582a1d659acc3c4b7174f8f00c13
Angie Quin <aquinones100@gmail.com>
2018-11-09 03:45:57 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { artistOne: "song one", artistTwo: "song two"};
 
-function updatePlaylist(playlist, artistOne, song three){
-playlist.artistOne = "song three"
-return playlist
+function updatePlaylist(playlist, artist, song){
+  playlist[artist] = "song"
+  return playlist
 }
